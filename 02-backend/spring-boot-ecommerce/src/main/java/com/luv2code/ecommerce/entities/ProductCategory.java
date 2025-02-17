09b4a463e20ce311265f81e659090c2d3e459748
Michael Harrington <mdharr7@gmail.com>
2025-02-17 13:44:40 +0800

    Build out basic database schema and spring boot project. Test REST APIs with Postman

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entities/ProductCategory.java
@@ -0,0 +1,78 @@
+package com.luv2code.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "products")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(Long id, String categoryName, Set<Product> products) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductCategory that = (ProductCategory) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                ", products=" + products +
+                '}';
+    }
+}
