0eadd3edc53cefec3640a682bae18649cbd2b299
Michael Harrington <mdharr7@gmail.com>
2025-02-20 15:54:32 +0800

    Create frontend models for State and Country. Stub out service methods for fetching countries and states from backend rest apis.

--- 03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,12 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { Country } from '../models/country';
+import { State } from '../models/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
 
-  constructor() { }
+	private countriesUrl = 'http://localhost:8080/api/countries';
+	private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
+
+	getCountries(): Observable<Country[]> {
+		return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+			map(response => response._embedded.countries)
+		);
+	}
+
+	getStates(countryCode: string): Observable<State[]> {
+		const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+		return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+			map(response => response._embedded.states)
+		)
+	}
 
 	getCreditCardMonths(startMonth: number): Observable<number[]> {
 
@@ -39,5 +58,17 @@ export class Luv2ShopFormService {
 		}
 
 		return of(data);
+	}
+}
+
+interface GetResponseCountries {
+	_embedded: {
+		countries: Country[];
+	}
+}
+
+interface GetResponseStates {
+	_embedded: {
+		states: State[];
 	}
 }
